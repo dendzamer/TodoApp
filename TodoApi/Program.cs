1397903387d72264e8a08c920b9eeb4fa6091d19
Denis Merdzic <3100deme@nibe.se>
2023-08-14 16:22:50 +0200

    This is the initial commit

---
+++ TodoApi/Program.cs
@@ -0,0 +1,26 @@
+using TodoApi.StartupConfig;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddStandradServices();
+builder.AddAuthServices();
+builder.AddHealthCheckServices();
+builder.AddCustomServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecks("/health").AllowAnonymous();
+
+app.Run();
---
+++ TodoApi/StartupConfig/DependencyInjectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TodoLibrary.DataAccess;
+
+namespace TodoApi.StartupConfig;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddStandradServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<ITodoData, TodoData>();
+    }
+
+    public static void AddHealthCheckServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHealthChecks()
+        .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+    }
+
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+        });
+
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+                };
+            });
+    }
+}
